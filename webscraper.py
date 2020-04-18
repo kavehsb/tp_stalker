4d3aa1fd9680b6f89c0092b34c4b2d68e9c125c6
Kaveh Buenaventura <kavehsbuen@gmail.com>
2020-04-18 16:22:47 -0700

    Include information for blank strings

--- webscraper.py
+++ webscraper.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-url = "https://www.walgreens.com/q/toilet+paper?N=3398"
+url = "https://www.walgreens.com/q/toilet+paper?N=1336"
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
@@ -11,9 +11,16 @@ for tp_data in content.find_all("div", attrs={"class": "wag-product-card-details
     unwanted = content.find_all("span", attrs={"class": "sr-only"})
     for span in unwanted:
         span.extract()
+
+    price = tp_data.find("span", attrs={"class": "product__price"}).text.strip()
+    availability = tp_data.find("p", attrs={"class": "wag-channal-availabilitytxt"}).text.strip()
+
+    if price == "":
+        price = "Price available in store"
+
     tp_object = {
-        "price": tp_data.find("span", attrs={"class": "product__price"}).text.strip(),
-        "availability": tp_data.find("p", attrs={"class": "wag-channal-availabilitytxt"}).text.strip()
+        "price": price,
+        "availability": availability,
     }
     data.append(tp_object)
 
