10ece5ee3f24375e4569297a7fdf53f09ae4aa9f
Kaveh Buenaventura <kavehsbuen@gmail.com>
2020-04-18 15:36:55 -0700

    Extract unnecessary html tags from prices

--- webscraper.py
+++ webscraper.py
@@ -8,9 +8,12 @@ content = BeautifulSoup(response.content, "html.parser")
 
 data = []
 for tp_data in content.find_all("div", attrs={"class": "wag-product-card-details wag-product-card-width_b"}):
+    unwanted = content.find_all("span", attrs={"class": "sr-only"})
+    for span in unwanted:
+        span.extract()
     tp_object = {
-        "price": tp_data.find("span", attrs={"class": "product__price"}).text,
-        "availability": tp_data.find("p", attrs={"class": "wag-channal-availabilitytxt"}).text
+        "price": tp_data.find("span", attrs={"class": "product__price"}).text.strip(),
+        "availability": tp_data.find("p", attrs={"class": "wag-channal-availabilitytxt"}).text.strip()
     }
     data.append(tp_object)
 
